45a607eb4d6f17f83334af516e966aef9f9e164a
keytel <201286@unamba.edu.pe>
2024-10-04 21:54:48 -0500

    v1.2.0

--- src/components/pokemon/ItemsPokemon.tsx
+++ src/components/pokemon/ItemsPokemon.tsx
@@ -21,11 +21,11 @@ export const ItemsPokemon =({pokemon}:Props)=> {
                 width={100}
                 height={100}
                 alt={pokemon.name}
+                priority={false}
             />
-            <h5 className="mb-1 text-xl font-medium text-gray-900">{name}</h5>
-            <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
-            <div className="flex mt-4 md:mt-6">
-                <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ver pokemon</a>
+            <h5 className="mt-4 text-xl font-medium text-gray-900">{name}</h5>
+            <div className="flex mt-2">
+                <a href={`/pokemon/details/${id}`} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ver pokemon</a>
             </div>
         </div>
     </div>
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,2 +1,3 @@
 export type {PokemonResponse} from '@/interfaces/pokemon/pokemon-responde';
 export type { SimplePokemon } from '@/interfaces/pokemon/simple-pokemon';
+export type { PokemonDetails } from '@/interfaces/pokemon/detailsPokemon';
